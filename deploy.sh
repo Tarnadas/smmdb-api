7aaf14435aa09833a3b87cfd06bac70124b44f3e
Mario Reder <mreder1289@gmail.com>
2020-04-09 13:27:12 +0200

    properly tag images

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,13 @@ docker pull $(grep -ioP '(?<=^from)\s+\S+' DockerfileBuild) &
 wait -n
 
 docker pull tarnadas/smmdb-api-build
-docker build --cache-from=tarnadas/smmdb-api-build -f ./DockerfileBuild .
+docker build --cache-from=tarnadas/smmdb-api-build -t tarnadas/smmdb-api-build:latest -f ./DockerfileBuild .
 docker push tarnadas/smmdb-api-build:latest
 docker tag tarnadas/smmdb-api-build tarnadas/smmdb-api-build:$GIT_HASH
 docker push tarnadas/smmdb-api-build:$GIT_HASH
 
 docker pull tarnadas/smmdb-api
-docker build --cache-from=tarnadas/smmdb-api .
+docker build --cache-from=tarnadas/smmdb-api -t tarnadas/smmdb-api .
 docker push tarnadas/smmdb-api:latest
 docker tag tarnadas/smmdb-api tarnadas/smmdb-api:$GIT_HASH
 docker push tarnadas/smmdb-api:$GIT_HASH
